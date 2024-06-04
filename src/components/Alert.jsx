d01dc4ef169022f194d0e465c5a780d6f632f8c0
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-06-04 16:08:44 +0700

    feat:done exs

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,25 @@
+import info from "../svg/alert-circle.svg";
+import warning from "../svg/alert-triangle.svg";
+import success from "../svg/check-circle.svg";
+import error from "../svg/frown.svg";
+
 // Start coding here
 
-function Alert() {}
+// Create a mapping of alert types to their corresponding SVGs
+const alertIcons = {
+  error,
+  warning,
+  info,
+  success,
+};
+
+function Alert({ alertType }) {
+  return (
+    <div className={`alert-${alertType}`}>
+      <img src={alertIcons[alertType]} alt={`${alertType} icon`} />
+      <div>This is {alertType} alert box</div>
+    </div>
+  );
+}
+
+export default Alert;
